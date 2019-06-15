02826ed897bc2afe3973c54667e0ce6d04e7a6f6
NguyenThuong <thuongnn1304@gmail.com>
2019-06-15 11:42:58 +0700

    fix finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,19 +24,14 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+   @user = User.new(user_params)
+   if @user.save
+    flash[:success] = "Sign up success"
+    redirect_to root_path
+    else
+    render 'new'
   end
-
+end
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
@@ -69,6 +64,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :age, :password)
+      params.require(:user).permit(:name, :email, :birthday, :gender , :password, :password_confirmation)
     end
-end
+  end
--- app/views/users/_user.json.jbuilder
+++ app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :name, :email, :age, :password_digest,:created_at, :updated_at
+json.extract! user, :id, :name, :email, :birthday, :gender, :password_digest,:created_at, :updated_at
 json.url user_url(user, format: :json)
